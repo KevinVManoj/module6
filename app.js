b37309b806fbfc6b9ba6287034b662d5ebd2502a
Kevin Manoj <kmanoj@uncc.edu>
2025-02-18 20:53:25 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+let port = 3000;
+let host = 'localhost';
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, host, () => {
+    console.log('Server is running on port', port);
+});
